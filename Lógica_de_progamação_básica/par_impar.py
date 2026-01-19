03d8909d136c4942faff19f345e1bedb461cf7f0
Rafael Toscan Gatelli <rafael.t.gatelli@gmail.com>
2026-01-19 18:45:33 -0300

    .

--- Lógica_de_progamação_básica/par_impar.py
+++ Lógica_de_progamação_básica/par_impar.py
@@ -3,7 +3,7 @@ numero = (input("Digite um numero:"))
 
 ultimo = numero[-1]
 
-if ultimo == "1" or "3" or "5" or "7" or "9":
+if ultimo in ["1" or "3" or "5" or "7" or "9"]:
    print("numero impar")
 else:
     print("numero par")
